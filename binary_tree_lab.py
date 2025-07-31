4795a27c51a4a3b7b60a31ab9c224283f9a2eaa1
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-31 16:49:04 -0600

    added max depth function

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import deque
 
 class TreeNode:
     def __init__(self, val: int):
@@ -8,8 +9,36 @@ class TreeNode:
 
 # TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if not root:
+        return 0
+    
+    queue = deque([root])
+    depth = 0
+
+    while queue:
+        level = len(queue)
+        for x in range(level):
+            current = queue.popleft()
+            print(current.val)
+
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+        depth += 1
+    return depth
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+print("Max Depth:", max_depth(root))
 
 # TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
     pass
+
